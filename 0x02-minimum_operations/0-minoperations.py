862ad10311977b28665b1ef758204836de3933c0
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-05-18 18:07:06 +0000

    minimum operations debugging one

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,14 +7,8 @@ to reach nH characters.
 
 def minOperations(n):
     operations = 0
-
-    # While n > 1, divide by 2 (equivalent to Copy)
     while n > 1:
         n //= 2
         operations += 1
-
-    # If n is > 1 after dividing, add an operation(Paste)
-    if n == 1:
-        operations += 1
-
+    operations += 1  # Account for the final Paste operation
     return operations
