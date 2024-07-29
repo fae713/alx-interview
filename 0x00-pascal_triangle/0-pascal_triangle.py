f547dbc48803a5c3df98df5141c0903358eca1c3
fae713 <favourmichael47@gmail.com>
2024-07-29 18:14:14 +0100

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal's Triangle.
 """
 
+
 def pascal_triangle(n):
     """
     A funtion that creates Pascal's Triangle up to n levels.
@@ -23,7 +24,7 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-        
+
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
